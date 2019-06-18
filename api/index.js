7bcee77269413a5ff3fc4af468eea8fb6f75e059
Sercan YILDIZ <ysercanz@gmail.com>
2019-06-18 12:24:03 +0200

    [update] add async serverStart

--- api/index.js
+++ api/index.js
@@ -8,18 +8,18 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const port = process.env.API_PORT || 3000
-
 const mongooseOptions = {
   useNewUrlParser: true
 }
 
-mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING, mongooseOptions)
-  .then(() => {
-    console.log('Database connection established!')
-  }, (err) => {
-    console.log('Error connecting Database instance due to:', err)
-  })
+const startServer = async () => {
+  await mongoose
+    .connect(process.env.MONGO_CONNECTION_STRING, mongooseOptions)
+    .then(() => console.log('Connected to database'))
+    .catch(err => console.log('Error connecting Database instance due to:', err) && process.exit(0))
 
-app.use('/action-todo', actionsTodo)
-app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+  app.use('/action-todo', actionsTodo)
+  app.listen(port, () => console.log(`Server running at http://localhost:${port}`))
+}
+
+startServer()
