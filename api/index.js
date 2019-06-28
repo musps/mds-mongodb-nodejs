febd2eaaad0e4e47e1154c84805e312f7321a903
Sercan YILDIZ <ysercanz@gmail.com>
2019-06-28 12:05:26 +0200

    [add] Ajout d'un healthcheck de la bdd avec mongoose

--- api/index.js
+++ api/index.js
@@ -4,31 +4,53 @@ const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const app = express()
 const actionsTodo = require('./actions/todo/index-router')
+const { sendError } = require('./actions/todo/index-utils')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 const mongooseOptions = {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  autoReconnect: true
 }
 
-const PORT = 3000
+let dbConnected = false
+const API_PORT = parseInt(process.env.API_PORT || 3000, 10)
 const MONGO_CONNECTION_STRING = (process.env.API_ENV === 'prod'
   ? process.env.PROD_MONGO_CONNECTION_STRING
   : process.env.DEV_MONGO_CONNECTION_STRING)
+
+if (!MONGO_CONNECTION_STRING) {
+  process.exit(0)
+}
 
 const startServer = () => {
   return new Promise(async (resolve, reject) => {
     await mongoose
       .connect(MONGO_CONNECTION_STRING, mongooseOptions)
-      .then(() => console.log('Connected to database'))
-      .catch(err => {
-        console.log('Error connecting Database instance due to:', err)
-        process.exit(0)
-      })
+      .then(() => console.log('connected') || (dbConnected = true))
+      .catch(() => console.log('not connected') || (dbConnected = false))
+
+    /**
+     * START: Healthcheck bdd
+     */
+    mongoose.connection.on('connected', () => console.log('connected') || (dbConnected = true))
+    mongoose.connection.on('error', () => console.log('error') || (dbConnected = false))
+    mongoose.connection.on('disconnected', () => console.log('disconnected') || (dbConnected = false))
+    // If the bdd is not connected we throw an error.
+    app.use(function (req, res, next) {
+      if (!dbConnected) {
+         sendError(res, 'Something went wrong', 503)
+        return
+      }
+      next();
+    })
+    /**
+     * STOP: Healthcheck bdd
+     */
 
     app.use('/action-todo', actionsTodo)
-    app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`))
+    app.listen(API_PORT, () => console.log(`Server running at http://localhost:${API_PORT}`))
     resolve({
       app,
       mongoose
